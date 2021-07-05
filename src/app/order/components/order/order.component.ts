a5c5c037e4f444123fe768ffb62e4f5a0e763dfd
Christianlopez4 <1107525635@u.icesi.edu.co>
2021-07-05 16:02:39 -0500

    Implemento la orden

---
+++ src/app/order/components/order/order.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../../product.model';
+import { CartService } from '../../../core/services/cart/cart.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators'; 
+
+@Component({
+  selector: 'app-order',
+  templateUrl: './order.component.html',
+  styleUrls: ['./order.component.css']
+})
+export class OrderComponent implements OnInit {
+
+  products$: Observable<Product[]>;
+
+  constructor(
+    private cartService: CartService
+  ) {
+    this.products$ = this.cartService.cart$;
+    /*
+    this.products$ = this.cartService.cart$.pipe(map((products:[]) => {
+      const distintos = [...new Set(products)];
+      return distintos;
+    }));
+    */
+   }
+
+  ngOnInit(): void {
+  }
+
+}
