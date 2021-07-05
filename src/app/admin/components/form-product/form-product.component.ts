61ff69bfa4218e318926266e9950f246fcb0fee2
Christianlopez4 <1107525635@u.icesi.edu.co>
2021-07-05 08:21:28 -0500

    Creo el FormGroup para crear productos

---
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { ProductsService } from '../../../core/services/products/products.service';
+
+@Component({
+  selector: 'app-form-product',
+  templateUrl: './form-product.component.html',
+  styleUrls: ['./form-product.component.css']
+})
+export class FormProductComponent implements OnInit {
+
+  form: FormGroup
+  
+  constructor(
+    private formBuilder: FormBuilder,
+    private productService: ProductsService,
+    private router: Router
+  ) { 
+    this.buildForm();
+  }
+
+  ngOnInit(): void {
+  }
+
+  buildForm() {
+    this.form = this.formBuilder.group({
+      id: ['', [Validators.required]],
+      title: ['', [Validators.required]],
+      price: [0, [Validators.required]],
+      image: [''],
+      description: ['', [Validators.required]]
+    });
+  }
+
+  saveProduct(event: Event) {
+    event.preventDefault();
+    if (this.form.valid) {
+      const product = this.form.value;
+      this.productService.createProduct(product)
+      .subscribe(res => {
+        console.log(res)
+        this.router.navigate(['./admin/products'])
+      })
+    }
+  }
+
+}
